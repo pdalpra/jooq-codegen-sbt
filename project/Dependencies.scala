9b1f1491ee05787bc466de5bef8826a5f2d97235
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2014-08-15 14:18:11 +0200

    Woops, wrong dependencies, need to depend on jooq-codegen

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  def jooq(version: String)     = "org.jooq" % "jooq"      % version
-  def jooqMeta(version: String) = "org.jooq" % "jooq-meta" % version
+  def jooqCodegen(version: String)     = "org.jooq" % "jooq-codegen" % version
 
-  def pluginDeps(version: String) = Seq(jooq _, jooqMeta _).map(_(version))
+  def pluginDeps(version: String) = Seq(jooqCodegen _).map(_(version))
 }
