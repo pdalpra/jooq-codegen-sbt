3c631722a7b36a23b45aad1943f7407516d94db3
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2014-09-02 00:13:46 +0200

    Format

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  private val scalaxb                      = "org.scalaxb" %% "scalaxb"      % "1.2.1"
+  private val scalaxb = "org.scalaxb" %% "scalaxb" % "1.2.1"
 
   def pluginDeps(version: String) = Seq(scalaxb)
 }
