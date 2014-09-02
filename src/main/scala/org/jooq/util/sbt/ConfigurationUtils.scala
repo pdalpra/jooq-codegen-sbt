0029927f069b7e4277c4151afad79bcd85928470
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2014-09-02 21:11:22 +0200

    Minor clean up

--- src/main/scala/org/jooq/util/sbt/ConfigurationUtils.scala
+++ src/main/scala/org/jooq/util/sbt/ConfigurationUtils.scala
@@ -6,22 +6,24 @@ import scala.xml.Elem
 
 import org.jooq.util.sbt.model._
 
+import scalaxb.CanWriteXML
+
 object ConfigurationUtils {
 
+  private def marshalToString[T: CanWriteXML](obj: T, label: String) = scalaxb.toXML(obj, label, defaultScope).toString()
+
   def writeConfigFile(xmlConfig: Option[(Elem, Elem)], codeConfig: Option[(Jdbc, Generator)], configFile: File): Unit = {
-    val (jdbcXmlString, generatorXmlString) = xmlConfig match {
-      case Some((jdbcXml, generatorXml)) => (jdbcXml.toString, generatorXml.toString)
+    val configFileContents = xmlConfig match {
+      case Some((jdbcXml, generatorXml)) => jdbcXml.toString + generatorXml.toString
       case None => codeConfig match {
         case Some((jdbc, generator)) =>
-          (scalaxb.toXML(jdbc, "jdbc", defaultScope).toString, scalaxb.toXML(generator, "generator", defaultScope).toString)
+          marshalToString(jdbc, "jdbc") + marshalToString(generator, "generator")
         case None =>
           throw new IllegalStateException("No configuration, whether through XML or code, is defined for JOOQ's generator.")
       }
     }
 
-    IO.delete(configFile)
-    IO.touch(configFile)
-    val xmlContent = Seq("<configuration>", jdbcXmlString, generatorXmlString, "</configuration>")
-    IO.writeLines(configFile, xmlContent, append = true)
+    val xmlContent = Seq("<configuration>", configFileContents, "</configuration>")
+    IO.writeLines(configFile, xmlContent)
   }
 }
